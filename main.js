f09756ddd12962cfe863170a39f8a9279d1149c5
Demian Tatay <demian.tatay@teamcmp.com>
2022-11-29 11:57:24 +0100

    hamburger menu done

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 function mobileNav() {
   const btn = document.querySelector('.hamburger')
+  const navItems = document.querySelector('.nav-items')
 
   btn.addEventListener('click', function() {
     btn.classList.toggle('open')
+    navItems.classList.toggle('open')
   })
 }
 
